ae8e4cca2a4c30fc749cf6d943d653a41527da0b
mstoro <mstoro@softserveinc.com>
2022-04-22 11:35:07 +0300

    [CCTRI-3510] implement refer/observables endpoint

--- code/api/enrich.py
+++ code/api/enrich.py
@@ -1,6 +1,6 @@
 from functools import partial
 
-from flask import g, Blueprint
+from flask import g, Blueprint, current_app
 
 from api.client import CensysClient
 from api.mapping import Sighting
@@ -34,5 +34,19 @@ def observe_observables():
 @enrich_api.route('/refer/observables', methods=['POST'])
 def refer_observables():
     _ = get_credentials()
-    _ = get_observables()
-    return jsonify_data([])
+    observables = get_observables()
+
+    relay_output = [
+        {
+            'id': (f'ref-censys-search-{observable["type"].replace("_", "-")}'
+                   f'-{observable["value"]}'),
+            'title': 'Events for this IP',
+            'description':
+                f'Events for this {observable["type"]} in the Censys',
+            'url': f'https://search.censys.io/hosts/{observable["value"]}/events',
+            'categories': ['Censys', 'Search', 'Events'],
+        }
+        for observable in observables
+    ]
+
+    return jsonify_data(relay_output)
